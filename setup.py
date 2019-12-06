c85a975f1bd76f891eca8fd32407c2e32613f435
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2019-12-06 13:51:08 +0000

    Convert binary-alexnet.

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     url="https://github.com/plumerai/larq-zoo",
     packages=find_packages(),
     license="Apache 2.0",
-    install_requires=["numpy~=1.15", "larq~=0.8.1", "zookeeper~=0.5.3"],
+    install_requires=["numpy>=1.15", "larq~=0.8.2", "zookeeper==1.0.dev6"],
     extras_require={
-        "tensorflow": ["tensorflow>=1.14.0"],
-        "tensorflow_gpu": ["tensorflow-gpu>=1.14.0"],
+        "tensorflow": ["tensorflow>=2.0.0"],
+        "tensorflow_gpu": ["tensorflow-gpu>=2.0.0"],
         "test": [
             "pytest>=4.3.1",
             "pytest-cov>=2.6.1",
@@ -29,10 +29,6 @@ setup(
             "scipy==1.3.3",
         ],
     },
-    entry_points="""
-        [console_scripts]
-        lqz=larq_zoo.train:cli
-    """,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
