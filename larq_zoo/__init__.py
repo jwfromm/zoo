95b8528df358ebf0c886d531885b41b9aec30c96
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2019-12-12 17:26:23 +0000

    Convert BinaryResNetE18.

--- larq_zoo/__init__.py
+++ larq_zoo/__init__.py
@@ -2,7 +2,7 @@ from larq_zoo.binarynet import BinaryAlexNet
 from larq_zoo.birealnet import BiRealNet
 
 # from larq_zoo.xnornet import XNORNet
-# from larq_zoo.resnet_e import BinaryResNetE18
+from larq_zoo.resnet_e import BinaryResNetE18
 from larq_zoo.densenet import (
     BinaryDenseNet28,
     BinaryDenseNet37,
@@ -19,7 +19,7 @@ __all__ = [
     "BinaryAlexNet",
     "BiRealNet",
     # "XNORNet",
-    # "BinaryResNetE18",
+    "BinaryResNetE18",
     "BinaryDenseNet28",
     "BinaryDenseNet37",
     "BinaryDenseNet37Dilated",
