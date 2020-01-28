88894df8103296ddf5a0b87820a7df6e6c0e0a99
Josh Fromm <jwfromm@uw.edu>
2020-01-28 14:26:11 -0800

    Added riptide resnet.

--- larq_zoo/riptide/__init__.py
+++ larq_zoo/riptide/__init__.py
@@ -1,9 +1,11 @@
 from larq_zoo.riptide.dorefanet import DoReFaNet
+from larq_zoo.riptide.resnet_e import BinaryResNetE18
 from larq_zoo.data import preprocess_input
 from larq_zoo.utils import decode_predictions
 
 __all__ = [
     "DoReFaNet",
+    "BinaryResNetE18",
     "decode_predictions",
     "preprocess_input",
 ]
